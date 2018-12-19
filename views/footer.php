3a0d2338aa418ef893c279bd2168428daa3154b5
Sourav Prasad <souravpd007@gmail.com>
2018-12-20 02:00:56 +0530

    Minor Modifications

--- views/footer.php
+++ views/footer.php
@@ -2,7 +2,7 @@
         <div class="row">
           <div class="col-12 col-md ">
             <img class="mb-2" src="favicon.ico" alt="" width="24" height="24">
-            <small class="d-block mb-3 text-muted">&copy; 2017-2018</small>
+            <small class="d-block mb-3 text-muted">&copy; By : Sourav Prasad</small>
           </div>
           <div class="col-6 col-md">
             <h5>Features</h5>
@@ -10,7 +10,7 @@
               <li><a class="text-muted" href="#">Cool stuff</a></li>
               <li><a class="text-muted" href="#">Random feature</a></li>
               <li><a class="text-muted" href="#">Team feature</a></li>
-              <li><a class="text-muted" href="#">Stuff for developers</a></li>
+              <li><a class="text-muted" href="#">Another Feature</a></li>
               <li><a class="text-muted" href="#">Another one</a></li>
               <li><a class="text-muted" href="#">Last time</a></li>
             </ul>
