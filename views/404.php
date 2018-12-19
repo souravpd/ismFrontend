6b8dd043d508ffafbaf159fd9db1d20c80a90853
Sourav Prasad <souravpd007@gmail.com>
2018-12-20 03:09:34 +0530

    added 404

---
+++ views/404.php
@@ -0,0 +1,9 @@
+<div class="container" style="padding:15px;">
+<div class="jumbotron">
+  <h1 class="display-4">4 0 4</h1>
+  <p class="lead">The following Webpage was not found on this server</p>
+  <hr class="my-4">
+  <p>Please check the link and try again</p>
+  <a class="btn btn-primary btn-lg" href="?page=home" role="button">Go Home</a>
+</div>
+</div>
